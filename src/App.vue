70b11366cfa9a791d67f2e0604b361b7d0aa8a3c
Александр Бабкин <_shurick@mail.ru>
2022-03-30 21:33:38 +0300

    login form test

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <v-app>
+    <v-app-bar color="primary"></v-app-bar>
+    <v-navigation-drawer color="grey-darken-1" permanent></v-navigation-drawer>
     <v-main>
-      <router-view></router-view>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout align-center justify-center>
+            <router-view></router-view>
+          </v-layout>
+        </v-container>
+      </v-content>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import {mapState} from 'vuex';
+import { mapState } from "vuex";
 
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-  },
+  components: {},
   computed: {
-    ...mapState(['user'])
-  }
-}
+    ...mapState(["user"]),
+  },
+};
 </script>
